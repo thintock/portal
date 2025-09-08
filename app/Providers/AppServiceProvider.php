ac55d3dbae723e11cdb1d9c1a392fb6953cb983a
Ubuntu <ubuntu@ip-172-31-41-9.ap-northeast-1.compute.internal>
2025-09-08 09:10:44 +0000

    chore: include Vite build assets for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +18,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(): void
     {
-        \URL::forceScheme('https');
+        URL::forceScheme('https');
     }
 }
